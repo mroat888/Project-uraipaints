93e9df7e8f6c81e19f9bd9e604e2fa72e50908d2
mroat888 <mroat07@gmail.com>
2022-01-27 17:40:12 +0700

    Update Layout

--- resources/views/layouts/masterLead.blade.php
+++ resources/views/layouts/masterLead.blade.php
@@ -26,27 +26,27 @@ License: You must have a valid license purchased only from themeforest to legall
         href="{{ asset('https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css') }}" />
 
     <!-- select2 CSS -->
-    <link href="{{ asset('public/template/vendors/select2/dist/css/select2.min.css') }}" rel="stylesheet"
+    <link href="{{ asset('/template/vendors/select2/dist/css/select2.min.css') }}" rel="stylesheet"
         type="text/css" />
 
     <!-- Data Table CSS -->
-    <link href="{{ asset('public/template/vendors/datatables.net-dt/css/jquery.dataTables.min.css') }}"
+    <link href="{{ asset('/template/vendors/datatables.net-dt/css/jquery.dataTables.min.css') }}"
         rel="stylesheet" type="text/css" />
-    <link href="{{ asset('public/template/vendors/datatables.net-responsive-dt/css/responsive.dataTables.min.css') }}"
+    <link href="{{ asset('/template/vendors/datatables.net-responsive-dt/css/responsive.dataTables.min.css') }}"
         rel="stylesheet" type="text/css" />
 
     <!-- Toggles CSS -->
-    <link href="{{ asset('public/template/vendors/jquery-toggles/css/toggles.css') }}" rel="stylesheet"
+    <link href="{{ asset('/template/vendors/jquery-toggles/css/toggles.css') }}" rel="stylesheet"
         type="text/css">
-    <link href="{{ asset('public/template/vendors/jquery-toggles/css/themes/toggles-light.css') }}" rel="stylesheet"
+    <link href="{{ asset('/template/vendors/jquery-toggles/css/themes/toggles-light.css') }}" rel="stylesheet"
         type="text/css">
 
     <!-- Morris Charts CSS -->
-    <link href="{{ asset('public/template/vendors/morris.js/morris.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/template/vendors/morris.js/morris.css') }}" rel="stylesheet" type="text/css" />
 
     <!-- Custom CSS -->
-    <link href="{{ asset('public/template/dist/css/style.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('public/template/dist/css/layout.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/template/dist/css/style.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/template/dist/css/layout.css') }}" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -63,7 +63,7 @@ License: You must have a valid license purchased only from themeforest to legall
             <a id="navbar_toggle_btn" class="navbar-toggle-btn nav-link-hover" href="javascript:void(0);"><span
                     class="feather-icon"><i data-feather="menu"></i></span></a>
             <a class="navbar-brand" href="index.html">
-                <img class="brand-img d-inline-block" src="{{ asset('public/images/logo.png') }}" alt="Uraipaint"
+                <img class="brand-img d-inline-block" src="{{ asset('/images/logo.png') }}" alt="Uraipaint"
                     style="max-height:30px;" />
                 {{-- URAI PAINTS --}}
             </a>
@@ -85,7 +85,7 @@ License: You must have a valid license purchased only from themeforest to legall
                                 <div class="media">
                                     <div class="media-img-wrap">
                                         <div class="avatar avatar-sm">
-                                            <img src="{{ asset('public/template/dist/img/avatar1.jpg') }}"
+                                            <img src="{{ asset('/template/dist/img/avatar1.jpg') }}"
                                                 alt="user" class="avatar-img rounded-circle">
                                         </div>
                                     </div>
@@ -103,7 +103,7 @@ License: You must have a valid license purchased only from themeforest to legall
                                 <div class="media">
                                     <div class="media-img-wrap">
                                         <div class="avatar avatar-sm">
-                                            <img src="{{ asset('public/template/dist/img/avatar1.jpg') }}"
+                                            <img src="{{ asset('/template/dist/img/avatar1.jpg') }}"
                                                 alt="user" class="avatar-img rounded-circle">
                                         </div>
                                     </div>
@@ -186,7 +186,7 @@ License: You must have a valid license purchased only from themeforest to legall
                         <div class="media">
                             <div class="media-img-wrap">
                                 <div class="avatar">
-                                    <img src="{{ asset('public/template/dist/img/avatar12.jpg') }}" alt="user"
+                                    <img src="{{ asset('/template/dist/img/avatar12.jpg') }}" alt="user"
                                         class="avatar-img rounded-circle">
                                 </div>
                                 <span class="badge badge-success badge-indicator"></span>
@@ -450,8 +450,8 @@ License: You must have a valid license purchased only from themeforest to legall
                     <button id="reset_settings" class="btn btn-success btn-block btn-reset mt-30">Reset</button>
                 </div>
             </div>
-            <img class="d-none" src="{{ asset('public/template/dist/img/logo-light.png') }}" alt="brand" />
-            <img class="d-none" src="{{ asset('public/template/dist/img/logo-dark.png') }}" alt="brand" />
+            <img class="d-none" src="{{ asset('/template/dist/img/logo-light.png') }}" alt="brand" />
+            <img class="d-none" src="{{ asset('/template/dist/img/logo-dark.png') }}" alt="brand" />
         </div>
         <!-- /Setting Panel -->
 
@@ -471,72 +471,72 @@ License: You must have a valid license purchased only from themeforest to legall
     <!-- /HK Wrapper -->
 
     <!-- jQuery -->
-    <script src="{{ asset('public/template/vendors/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/jquery/dist/jquery.min.js') }}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="{{ asset('public/template/vendors/popper.js/dist/umd/popper.min.js') }}"></script>
-    <script src="{{ asset('public/template/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
     <!-- Slimscroll JavaScript -->
-    <script src="{{ asset('public/template/dist/js/jquery.slimscroll.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/jquery.slimscroll.js') }}"></script>
 
     <!-- Fancy Dropdown JS -->
-    <script src="{{ asset('public/template/dist/js/dropdown-bootstrap-extended.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/dropdown-bootstrap-extended.js') }}"></script>
 
     <!-- Morris Charts JavaScript -->
-    <script src="{{ asset('public/template/vendors/raphael/raphael.min.js') }}"></script>
-    <script src="{{ asset('public/template/vendors/morris.js/morris.min.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/linecharts-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/morris.js/morris.min.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/linecharts-data.js') }}"></script>
 
     <!-- EChartJS JavaScript -->
-    <script src="{{ asset('public/template/vendors/echarts/dist/echarts-en.min.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/piecharts-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/echarts/dist/echarts-en.min.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/piecharts-data.js') }}"></script>
 
     <!-- Easy pie chart JS -->
-    <script src="{{ asset('public/template/vendors/easy-pie-chart/dist/jquery.easypiechart.min.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/easypiechart-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/easy-pie-chart/dist/jquery.easypiechart.min.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/easypiechart-data.js') }}"></script>
 
     <!-- FeatherIcons JavaScript -->
-    <script src="{{ asset('public/template/dist/js/feather.min.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/feather.min.js') }}"></script>
 
     <!-- Toggles JavaScript -->
-    <script src="{{ asset('public/template/vendors/jquery-toggles/toggles.min.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/toggle-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/jquery-toggles/toggles.min.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/toggle-data.js') }}"></script>
 
     <!-- Counter Animation JavaScript -->
-    <script src="{{ asset('public/template/vendors/waypoints/lib/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('public/template/vendors/jquery.counterup/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/jquery.counterup/jquery.counterup.min.js') }}"></script>
 
     <!-- Sparkline JavaScript -->
-    <script src="{{ asset('public/template/vendors/jquery.sparkline/dist/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/jquery.sparkline/dist/jquery.sparkline.min.js') }}"></script>
 
     <!-- Vector Maps template -->
-    <script src="{{ asset('public/template/vendors/vectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
-    <script src="{{ asset('public/template/vendors/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/vectormap-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/vectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/vectormap-data.js') }}"></script>
 
     <!-- Owl JavaScript -->
-    <script src="{{ asset('public/template/vendors/owl.carousel/dist/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/owl.carousel/dist/owl.carousel.min.js') }}"></script>
 
     <!-- Owl Init JavaScript -->
-    <script src="{{ asset('public/template/dist/js/owl-data.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/owl-data.js') }}"></script>
 
     <!-- Select2 JavaScript -->
-    <script src="{{ asset('public/template/vendors/select2/dist/js/select2.full.min.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/select2-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/select2/dist/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/select2-data.js') }}"></script>
 
     <!-- Bootstrap Tagsinput JavaScript -->
-    {{-- <script src="{{asset('public/template/vendors/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js')}}"></script> --}}
+    {{-- <script src="{{asset('/template/vendors/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js')}}"></script> --}}
 
     <!-- Jasny-bootstrap  JavaScript -->
-    <script src="{{ asset('public/template/vendors/jasny-bootstrap/dist/js/jasny-bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/jasny-bootstrap/dist/js/jasny-bootstrap.min.js') }}"></script>
 
     <!-- Data Table JavaScript -->
-    <script src="{{ asset('public/template/vendors/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('public/template/vendors/datatables.net-responsive/js/dataTables.responsive.min.js') }}">
+    <script src="{{ asset('/template/vendors/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/template/vendors/datatables.net-responsive/js/dataTables.responsive.min.js') }}">
     </script>
-    <script src="{{ asset('public/template/dist/js/dataTables-data.js') }}"></script>
-    <script src="{{ asset('public/template/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('/template/dist/js/dataTables-data.js') }}"></script>
+    <script src="{{ asset('/template/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 
 
     <script src="https://cdn.jsdelivr.net/npm/moment@2.27.0/moment.min.js"></script>
@@ -545,8 +545,8 @@ License: You must have a valid license purchased only from themeforest to legall
 
 
     <!-- Init JavaScript -->
-    <script src="{{ asset('public/template/dist/js/init.js') }}"></script>
-    <script src="{{ asset('public/template/dist/js/gmap-data.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/init.js') }}"></script>
+    <script src="{{ asset('/template/dist/js/gmap-data.js') }}"></script>
 
     <script src="{{ asset('https://unpkg.com/sweetalert/dist/sweetalert.min.js')}}"></script>
 
