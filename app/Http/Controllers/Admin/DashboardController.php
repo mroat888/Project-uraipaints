491005400586efad7ed266e0eb1bcc09b86c9a42
mroat888 <mroat07@gmail.com>
2022-02-21 22:46:07 +0700

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -27,17 +27,21 @@ class DashboardController extends Controller
         $monthly_plans = DB::table('monthly_plans')
         ->join('users', 'users.id', 'monthly_plans.created_by')
         ->where('monthly_plans.status_approve', 2)
-        ->where('users.team_id', Auth::user()->team_id)
+        ->whereYear('month_date', $year)
+        ->whereMonth('month_date', $month)
         ->select(
             'monthly_plans.*'
         )
         ->get();
 
         $data['count_monthly_plans'] = 0;
-        // -- นับจำนวน slaeplans
-        $data['count_sale_plans_result'] = 0;
         $data['count_cust_new_amount'] = 0;
         $data['count_cust_visits_amount'] = 0;
+        
+        // -- นับจำนวน slaeplans
+        $data['count_sale_plans_result'] = 0;
+        $data['count_shops_saleplan_result'] = 0;
+        $data['count_visit_results_result'] = 0;
         foreach($monthly_plans as $monthly_plan){
             $data['count_monthly_plans'] = $data['count_monthly_plans'] + $monthly_plan->sale_plan_amount ; // 	จำนวนแผนงาน
             $data['count_cust_new_amount'] = $data['count_cust_new_amount'] + $monthly_plan->cust_new_amount ; // จำนวนลูกค้าใหม่
@@ -48,12 +52,11 @@ class DashboardController extends Controller
             foreach($sale_plans as $sp_value){
                 $check_result = DB::table('sale_plan_results')->where('sale_plan_id', $sp_value->id)->first();
                 if(!is_null($check_result)){
-                    $data['count_sale_plans_result'] = $data['count_sale_plans_result'] + 1;
+                    $data['count_sale_plans_result'] = $data['count_sale_plans_result'] +1 ;
                 }
             }
-
+            
             // -- นับจำนวน ลูกค้าใหม่
-            $data['count_shops_saleplan_result'] = 0;
             $customer_shops_saleplan = DB::table('customer_shops_saleplan')->where('monthly_plan_id', $monthly_plan->id)->get();
             foreach($customer_shops_saleplan as $sp_value){
                 $check_result = DB::table('customer_shops_saleplan_result')->where('customer_shops_saleplan_id', $sp_value->id)->first();
@@ -61,31 +64,20 @@ class DashboardController extends Controller
                     $data['count_shops_saleplan_result'] = $data['count_shops_saleplan_result'] + 1;
                 }
             }
-
+            
             // -- นับจำนวน ลูกค้าเยี่ยม
-            $data['count_isit_results_result'] = 0;
             $customer_visits = DB::table('customer_visits')->where('monthly_plan_id', $monthly_plan->id)->get();
             foreach($customer_visits as $sp_value){
                 $check_result = DB::table('customer_visit_results')->where('customer_visit_id', $sp_value->id)->first();
                 if(!is_null($check_result)){
-                    $data['count_isit_results_result'] = $data['count_isit_results_result'] + 1;
+                    $data['count_visit_results_result'] = $data['count_visit_results_result'] + 1;
                 }
             }
-
+            
         }
-
-        $monthly_plans = DB::table('monthly_plans')
-        ->join('users', 'users.id', 'monthly_plans.created_by')
-        ->where('monthly_plans.status_approve', 2)
-        ->where('users.team_id', Auth::user()->team_id)
-        ->select(
-            'monthly_plans.*'
-        )
-        ->get();
-
+    
         $data['list_approval'] = DB::table('assignments')
             ->join('users', 'assignments.created_by', '=', 'users.id')
-            ->where('users.team_id', Auth::user()->team_id)
             ->whereMonth('assignments.assign_request_date', Carbon::now()->format('m'))
             ->whereIn('assignments.assign_status', [0,1,2])
             ->where('users.status', 1) // สถานะ 1 = salemam, 2 = lead , 3 = head , 4 = admin
@@ -93,19 +85,18 @@ class DashboardController extends Controller
 
 
         $data['assignments'] = DB::table('assignments')
-            ->where('created_by', Auth::user()->id)
             ->whereMonth('assign_work_date', Carbon::now()->format('m'))
             ->where('assign_status', 3)
             ->get();
         
-        $data['notes'] = Note::where('employee_id', Auth::user()->id)->whereMonth('note_date', Carbon::now()->format('m'))->get();
-        $data['customer_shop'] = Customer::where('created_by', Auth::user()->team_id)->where('shop_status', 0)->whereMonth('created_at', Carbon::now()->format('m'))->get();
+        $data['notes'] = Note::whereMonth('note_date', Carbon::now()->format('m'))->get();
+        $data['customer_shop'] = Customer::where('shop_status', 0)->whereMonth('created_at', Carbon::now()->format('m'))->get();
 
         $api_token = $this->api_token->apiToken();
         $data['api_token'] = $api_token;
 
         //-- หาจำนวนร้านค้าใน ทีม
-        $user_teams = DB::table('users')->where('status', 1)->where('team_id', Auth::user()->team_id)->get();
+        $user_teams = DB::table('users')->where('status', 1)->get();
 
         $data['sum_CustTotal'] = 0;
         $data['sum_ActiveTotal'] = 0;
@@ -117,7 +108,6 @@ class DashboardController extends Controller
         $data['sum_totalAmtSale'] = 0; // เป้ายอดขายปีปัจจุบัน
 
         foreach($user_teams as $team){
-            
             $response = Http::withToken($api_token)
             ->get('http://49.0.64.92:8020/api/v1/sellers/'.$team->api_identify.'/dashboards', [
                 'year' => $year,
