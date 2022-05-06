7c2e2bb1f57bf4e6ae7c8ce793320c872f2c226f
mroat888 <mroat07@gmail.com>
2022-05-06 09:48:52 +0700

    update

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -106,10 +106,12 @@ class CustomerController extends Controller
             'province.PROVINCE_NAME',
             'customer_shops_saleplan_result.*',
             'customer_shops_saleplan.*',
+            'customer_shops_saleplan.id as saleplan_id',
             'customer_shops_saleplan.shop_aprove_status as saleplan_shop_aprove_status',
             'customer_shops.*'
         )
-        ->orderBy('customer_shops_saleplan.id', 'desc')
+        ->groupBy('customer_shops.id')
+        ->orderBy('customer_shops_saleplan.monthly_plan_id', 'desc')
         ->get();
 
         $data['customer_shops'] = $customer_shops;
