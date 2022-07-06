9a3ce9d60f9028dfda86ffb3e2021a00a7ca4342
mroat888 <mroat07@gmail.com>
2022-07-06 08:33:16 +0700

    update

--- resources/views/headManager/approval_general_history.blade.php
+++ resources/views/headManager/approval_general_history.blade.php
@@ -11,7 +11,6 @@
     $action_search = "head/approvalgeneral/history/search"; //-- action form
 @endphp
 
-
 <!-- Breadcrumb -->
 <nav class="hk-breadcrumb" aria-label="breadcrumb">
     <ol class="breadcrumb breadcrumb-light bg-transparent">
@@ -23,13 +22,14 @@
 
 <!-- Container -->
 <div class="container-fluid px-xxl-65 px-xl-20">
+
     <!-- Title -->
     <div class="hk-pg-header mb-10">
         <div class="topichead-bgred"><i data-feather="file-text"></i> {{ $title_header }}</div>
     </div>
     <!-- /Title -->
 
-    <!-- Row -->
+<!-- Row -->
 <div class="row">
     <div class="col-xl-12">
         <section class="hk-sec-wrapper">
